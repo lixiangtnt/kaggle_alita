f0da41eecae1d3fecc8677f12bc7005b5707efa8
xiangli <1124lx@gmail.com>
2019-03-10 10:40:46 +0800

    samples

--- data_utils.py
+++ data_utils.py
@@ -1,16 +1,27 @@
 import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn import linear_model
+
 
 def load_data():
-
     test_data_path = "data/test.csv"
     train_data_path = "data/train.csv"
     test_data = pd.read_csv(test_data_path)
     train_data = pd.read_csv(train_data_path)
-    train_Y = train_data.loc[:, "target"]
-    train_X = train_data.iloc[:, 2:]
-    test_X = test_data[:, 1]
+    train_y = train_data.loc[:, "target"]
+    train_x = train_data.iloc[:, 2:]
+    test_x = test_data.iloc[:, 1:]
 
-    return train_X, train_Y, test_X
+    return train_x, train_y, test_x
+
+
+# train_X, train_Y, test_X = load_data()
+# X_train, X_valid, Y_train, Y_valid = train_test_split(train_X, train_Y, test_size=0.1, random_state=42)
+# clf = linear_model.SGDClassifier(max_iter=1000, tol=1e-3)
+# clf.fit(X_train, Y_train)
+# result = clf.predict(X_valid)
+# result_df = pd.DataFrame(data={"result": result, "ground_truth": Y_valid})
+# result_df.to_csv("svm_baseline.csv", sep="\t", index=False)
 
 
 
